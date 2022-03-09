c1f6d56fcc0f72be8ce4db04ddd79dfeffba85c9
sagivma <sagivm01@gmail.com>
2022-03-09 23:12:02 +0200

    finish payment stub functionality

--- paymentStub/main.py
+++ paymentStub/main.py
@@ -6,22 +6,15 @@ if __name__ == '__main__':
         "Content-Type": "application/json"
     }
 
-    url_payment_method = "http://127.0.0.1:5000/remove_user_credit_card/<string:userId>"
+    url_payment_method = "http://127.0.0.1:5000/make_payment"
     payload_payment_method = {
-        "creditToken": "762b807a-0ff3-5a7d-868d-eb1616747af9",
+        "userId": "<string:userId>",
+        "storeId": "<string:storeId>",
+        "creditCardToken": "ed99d320-1d86-59db-b579-054796b766e2",
+        "creditAmount": 30,
+        "eWalletToken": "98ea3c23-9379-534d-befc-187bf8d38677",
+        "cashBackAmount": 20
     }
 
     response = requests.request("UPDATE", url_payment_method, json=payload_payment_method, headers=headers)
     print(response.text)
-
-    # url_payment = "http://127.0.0.1:5000/make_web_payment"
-    # payload_payment = {
-    #     "returnUrl": "https://www.yoursite.com/thankyou.php",
-    #     "errorUrl": "https://www.yoursite.com/oops.php",
-    #     "cancelUrl": "https://www.yoursite.com/seeYouNextTime.php",
-    #     "cardId": str(response.json()['cardId']),
-    #     "accountId": "33612345678",
-    #     "totalAmount": "1500",
-    # }
-    # response = requests.request("POST", url_payment, json=payload_payment, headers=headers)
-    # print(response.text)
--- paymentStub/payment.py
+++ paymentStub/payment.py
@@ -1,20 +1,22 @@
 import uuid
+from decimal import Decimal
+from uuid import uuid5
 
 import botocore
 from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
-from flask import Flask, render_template
+from flask import Flask
 from flask import jsonify
 from flask import request
-from uuid import uuid5, uuid4
-import random
 import boto3
 
 # creating of object of flask app
 app = Flask(__name__)
+
 # creating dynamodb access
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-table = dynamodb.Table('PaymentStub')
+consumersTable = dynamodb.Table('ConsumersPaymentStub')
+storesTable = dynamodb.Table('StoresPaymentStub')
 
 
 @app.route('/create_user_e_wallet/<string:userId>', methods=['POST'])
@@ -22,7 +24,7 @@ def create_user_e_wallet(userId):
     try:
         e_wallet_token = str(uuid5(uuid.NAMESPACE_URL, userId))
         item = {"userId": userId, "creditCards": {},  "e_wallet": {e_wallet_token: 0}}
-        table.put_item(Item=item, ConditionExpression='attribute_not_exists(userId)')
+        consumersTable.put_item(Item=item, ConditionExpression='attribute_not_exists(userId)')
         result = {"token": e_wallet_token, "msg": "created user e_wallet successfully"}
         return jsonify(result), 200
     except ClientError as e:
@@ -38,12 +40,17 @@ def create_user_e_wallet(userId):
 def add_user_credit_card(userId):
     try:
         data = request.get_json()
-        cardNumber = data["cardNumber"]
-        expiryDate = data["expiryDate"]
-        cvv = data["cvv"]
+        try:
+            cardNumber = data["cardNumber"]
+            expiryDate = data["expiryDate"]
+            cvv = data["cvv"]
+        except KeyError:
+            result = {"token": None, "msg": "Bad Arguments Request"}
+            return jsonify(result), 400
+
         # TODO: checks for valid credit data
         credit_card_token = str(uuid5(uuid.NAMESPACE_URL, cardNumber))
-        table.update_item(
+        consumersTable.update_item(
             Key={"userId": userId},
             UpdateExpression="SET creditCards.#cardToken = :amount",
             ExpressionAttributeNames={"#cardToken": credit_card_token},
@@ -61,8 +68,13 @@ def add_user_credit_card(userId):
 def remove_user_credit_card(userId):
     try:
         data = request.get_json()
-        credit_card_token = data["creditToken"]
-        table.update_item(
+        try:
+            credit_card_token = data["creditToken"]
+        except KeyError:
+            result = {"msg": "Bad Arguments Request"}
+            return jsonify(result), 400
+
+        consumersTable.update_item(
             Key={"userId": userId},
             UpdateExpression="REMOVE creditCards.#cardToken",
             ExpressionAttributeNames={"#cardToken": credit_card_token},
@@ -71,7 +83,7 @@ def remove_user_credit_card(userId):
         result = {"msg": "removed user credit card successfully"}
         return jsonify(result), 200
     except botocore.exceptions.ClientError as e:
-        result = {"token": None, "msg": e.response['Error']['Message']}
+        result = {"msg": e.response['Error']['Message']}
         return jsonify(result), 403
 
 
@@ -79,89 +91,130 @@ def remove_user_credit_card(userId):
 def e_wallet_balance(userId):
     try:
         data = request.get_json()
-        e_wallet_token = data["eWalletToken"]
-        table.query(
-            KeyConditionExpression=Key('userId').eq(userId)
+        try:
+            e_wallet_token = data["eWalletToken"]
+        except KeyError:
+            result = {"balance": None, "msg": "Bad Arguments Request"}
+            return jsonify(result), 400
 
-        )
-        result = {"msg": "removed user credit card successfully"}
-        return jsonify(result), 200
-    except botocore.exceptions.ClientError as e:
-        result = {"token": None, "msg": e.response['Error']['Message']}
-        return jsonify(result), 403
-
-
-def __update_e_wallet_balance__(userId, e_wallet_token, amount):
-    try:
-        table.update_item(
-            Key={"userId": userId},
-            UpdateExpression="SET e_wallet.#eWalletToken += :amount",
+        response = consumersTable.query(
+            KeyConditionExpression=Key('userId').eq(userId),
+            FilterExpression="attribute_exists(e_wallet.#eWalletToken)",
             ExpressionAttributeNames={"#eWalletToken": e_wallet_token},
-            ExpressionAttributeValues={":amount": amount},
-            ConditionExpression="e_wallet.#eWalletToken + :amount >= 0",
         )
-        result = {"msg": "updated e_wallet balance successfully"}
+        if not response['Items']:
+            result = {"balance": None, "msg": "invalid user id or e_wallet token"}
+            return jsonify(result), 403
+        userData = response['Items'][0]
+        e_wallet = userData["e_wallet"]
+        balance = str(e_wallet[e_wallet_token])
+        result = {"balance": balance, "msg": "returned user e_wallet balance successfully"}
         return jsonify(result), 200
     except botocore.exceptions.ClientError as e:
-        result = {"token": None, "msg": e.response['Error']['Message']}
+        result = {"balance": None, "msg": e.response['Error']['Message']}
         return jsonify(result), 403
 
 
-@app.route('/make_web_payment', methods=['POST'])
-def make_web_payment():
-    payload = request.json
-    returnUrl = payload["returnUrl"]
-    errorUrl = payload["errorUrl"]
-    cancelUrl = payload["cancelUrl"]
-    accountId = payload["accountId"]
-    cardId = payload["cardId"]
-    totalAmount = payload["totalAmount"]
+@app.route('/save_store_bank_account/<string:storeId>', methods=['POST'])
+def save_store_bank_account(storeId):
+    try:
+        data = request.get_json()
+        try:
+            bank_account = data["bankAccount"]
+        except KeyError:
+            result = {"token": None, "msg": "Bad Arguments Request"}
+            return jsonify(result), 400
 
-    print("web payment request - charge " + cardId +
-          " and credit account " + accountId + " for the amount " + totalAmount)
-    if int(totalAmount) <= 0:
-        return render_template('exception.html', error="total amount must be positive"), 401
-    if cardId == str(100):
-        return render_template('exception.html', error="special sign"), 401
-
-    rand_token = uuid4()
-    res = {"token": rand_token}
-    return jsonify(res), 200
-
-
-@app.route('/make_digital_payment', methods=['POST'])
-def make_digital_payment():
-    payload = request.json
-    walletId = payload["walletId"]
-    accountId = payload["accountId"]
-    totalAmount = payload["totalAmount"]
-
-    print("digital payment request - charge wallet " + walletId +
-          " and credit account " + accountId + " for the amount " + totalAmount)
-    if int(totalAmount) <= 0:
-        return render_template('exception.html', error="total amount must be positive"), 401
-    if walletId == str(100):
-        return render_template('exception.html', error="special sign"), 401
-
-    rand_token = uuid4()
-    res = {"token": rand_token}
-    return jsonify(res)
+        bank_account_token = str(uuid5(uuid.NAMESPACE_URL, bank_account))
+        item = {"storeId": storeId, "bankAccount": bank_account_token}
+        storesTable.put_item(Item=item, ConditionExpression='attribute_not_exists(storeId)')
+        result = {"token": bank_account_token, "msg": "save store bank account number successfully"}
+        return jsonify(result), 200
+    except ClientError as e:
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            result = {"token": None, "msg": "store already has updated bank account number"}
+            return jsonify(result), 403
+        else:
+            result = {"token": None, "msg": e.response['Error']['Message']}
+            return jsonify(result), 403
 
 
-@app.route('/create_payment_method', methods=['POST'])
-def create_payment_method():
-    payload = request.json
-    accountId = payload["accountId"]
-    cardNumber = payload["cardNumber"]
-    cardDate = payload["cardDate"]
+@app.route('/delete_store_bank_account/<string:storeId>', methods=['DELETE'])
+def delete_store_bank_account(storeId):
+    try:
+        data = request.get_json()
+        try:
+            bank_account_token = data["bankAccountToken"]
+        except KeyError:
+            result = {"msg": "Bad Arguments Request"}
+            return jsonify(result), 400
 
-    print("create payment method request - account id "
-          + accountId + " card number " + cardNumber + " cardDate " + cardDate)
-    if accountId == str(100):
-        return render_template('exception.html', error="special sign"), 401
+        storesTable.delete_item(
+            Key={"storeId": storeId},
+            ConditionExpression="bankAccount = :bankAccountToken",
+            ExpressionAttributeValues={":bankAccountToken": bank_account_token},
+        )
+        result = {"msg": "deleted store's bank account successfully"}
+        return jsonify(result), 200
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            result = {"msg": "invalid bank account token"}
+            return jsonify(result), 403
+        result = {"msg": e.response['Error']['Message']}
+        return jsonify(result), 403
 
-    res = {"cardId": random.randint(1, 256)}
-    return jsonify(res)
+
+@app.route('/make_payment', methods=['UPDATE'])
+def make_payment():
+    try:
+        data = request.get_json()
+        try:
+            userId = data["userId"]
+            storeId = data["storeId"]
+            credit_card_token = data["creditCardToken"]
+            credit_amount = data["creditAmount"]
+            e_wallet_token = data["eWalletToken"]
+            cash_back_amount = data["cashBackAmount"]
+            if cash_back_amount < 0 or credit_amount < 0:
+                raise KeyError
+        except KeyError:
+            result = {"msg": "Bad Arguments Request"}
+            return jsonify(result), 400
+
+        store_response = storesTable.query(KeyConditionExpression=Key('storeId').eq(storeId))
+        if len(store_response['Items']) == 0:
+            result = {"msg": "store not has bank account number for payments"}
+            return jsonify(result), 403
+
+        consumersTable.update_item(
+            Key={"userId": userId},
+            UpdateExpression="SET creditCards.#creditCardToken = creditCards.#creditCardToken - :creditAmount,"
+                             " e_wallet.#eWalletToken = e_wallet.#eWalletToken - :cashBackAmount",
+            ExpressionAttributeNames={"#creditCardToken": credit_card_token, "#eWalletToken": e_wallet_token},
+            ExpressionAttributeValues={":creditAmount": Decimal(str(credit_amount)),
+                                       ":cashBackAmount": Decimal(str(cash_back_amount))},
+            ConditionExpression="creditCards.#creditCardToken >= :creditAmount AND"
+                                " e_wallet.#eWalletToken >= :cashBackAmount",
+        )
+
+        total_amount = credit_amount + cash_back_amount if cash_back_amount is not None else 0
+        cash_back_percentage = 0.1
+        consumersTable.update_item(
+            Key={"userId": userId},
+            UpdateExpression="SET e_wallet.#eWalletToken = e_wallet.#eWalletToken + :cashBackAmount",
+            ExpressionAttributeNames={"#eWalletToken": e_wallet_token},
+            ExpressionAttributeValues={":cashBackAmount": Decimal(str(total_amount * cash_back_percentage))},
+        )
+
+        result = {"msg": "payment made successfully"}
+        return jsonify(result), 200
+
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            result = {"msg": "user has no enough credit card / cash-back money"}
+            return jsonify(result), 403
+        result = {"msg": e.response['Error']['Message']}
+        return jsonify(result), 403
 
 
 if __name__ == '__main__':
