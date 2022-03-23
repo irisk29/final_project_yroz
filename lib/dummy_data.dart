c99cd096f0243d53572f8855ffbe0e45a8960df1
Iris Kronfeld <iris.kronfeld@gmail.com>
2022-03-23 20:59:13 +0200

    add new categories

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -6,24 +6,61 @@ const DUMMY_CATEGORIES = const [
   Category(
     id: 'c1',
     title: 'Food',
-    color: Colors.pinkAccent,
+    color: Color.fromARGB(255, 240, 132, 168),
+    image: AssetImage("assets/images/food.png")
   ),
   Category(
     id: 'c2',
     title: 'Home',
-    color: Colors.yellow,
+    color: Color.fromARGB(255, 18, 107, 110),
+    image: AssetImage("assets/images/home.jpeg")
+  ),
+  Category(
+    id: 'c3',
+    title: 'Sports',
+    color: Color.fromARGB(255, 104, 104, 103),
+    image: AssetImage("assets/images/sports.jpeg")
+  ),
+  Category(
+    id: 'c4',
+    title: 'Clothes',
+    color: Color.fromARGB(255, 255, 177, 59),
+    image: AssetImage("assets/images/clothes.png")
+  ),
+  Category(
+    id: 'c5',
+    title: 'Shoes',
+    color: Color.fromARGB(255, 250, 45, 37),
+    image: AssetImage("assets/images/shoes.jpeg")
+  ),
+  Category(
+    id: 'c6',
+    title: 'Accessories',
+    color: Color.fromARGB(255, 212, 191, 1),
+    image: AssetImage("assets/images/accessories.jpeg")
+  ),
+  Category(
+    id: 'c7',
+    title: 'Electronics',
+    color: Color.fromARGB(255, 6, 134, 240),
+    image: AssetImage("assets/images/electronics.jpeg")
+  ),
+  Category(
+    id: 'c8',
+    title: 'Art',
+    color: Color.fromARGB(255, 197, 110, 255),
+    image: AssetImage("assets/images/art.png")
+  ),
+  Category(
+    id: 'c9',
+    title: 'Cleaning',
+    color: Color.fromARGB(255, 122, 255, 81),
+    image: AssetImage("assets/images/cleaning.jpeg")
+  ),
+  Category(
+    id: 'c10',
+    title: 'Sweets',
+    color: Color.fromARGB(255, 255, 16, 215),
+    image: AssetImage("assets/images/sweets.jpeg")
   ),
 ];
-
-// const DUMMY_STORES = const [
-//   Store(
-//     id: 's1',
-//     title: 'Chocolate',
-//     address: 'GOOD ADDRESS',
-//   ),
-//   Store(
-//     id: 's2',
-//     title: 'Furniture',
-//     address: 'BAD ADDRESS',
-//   ),
-// ];
