2f18bab76013696b350cb62dd5c35bbf985105bb
sagivma <sagivm01@gmail.com>
2022-04-14 19:19:45 +0300

    improve feedback ui

--- lib/screens/feedback_screen.dart
+++ lib/screens/feedback_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:final_project_yroz/screens/account_screen.dart';
 import 'package:final_project_yroz/screens/tabs_screen.dart';
 import 'package:flutter_mailer/flutter_mailer.dart';
 import 'package:f_logs/f_logs.dart';
@@ -25,14 +24,16 @@ class _FeedBackScreen extends State<FeedBackScreen> {
   late List<Tuple2<int, int>> _ratings;
   List<String> questions = [
     "The app is user friendly",
-    "The app did not crash",
+    "The app is working properly",
     "The app responds fast",
-    "The app's design"
+    "The app's design looks good"
   ];
+  var _formChanged;
 
   @override
   void didChangeDependencies() {
-    final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     widget.userEmail = routeArgs['email'] as String;
     super.didChangeDependencies();
   }
@@ -41,14 +42,17 @@ class _FeedBackScreen extends State<FeedBackScreen> {
   void initState() {
     super.initState();
     _ratings = [];
+    _formChanged = false;
   }
 
   Future<void> sendEmail() async {
     String finalText = "";
-    Secret secret = await SecretLoader(secretPath: "assets/secrets.json").load();
+    Secret secret =
+        await SecretLoader(secretPath: "assets/secrets.json").load();
 
     for (Tuple2<int, int> item in _ratings) {
-      finalText += "Question: ${questions[item.item1]}\nRating: ${item.item2}\n\n";
+      finalText +=
+          "Question: ${questions[item.item1]}\nRating: ${item.item2}\n\n";
     }
     String freeText = _textEditingController.text;
     if (freeText.isNotEmpty) {
@@ -58,8 +62,11 @@ class _FeedBackScreen extends State<FeedBackScreen> {
     String companyEmail = secret.COMPANY_EMAIL;
     String subject = "FeedBack From User ${widget.userEmail}";
     print(companyEmail);
-    final MailOptions mailOptions =
-        MailOptions(body: finalText, subject: subject, recipients: [companyEmail], isHTML: false);
+    final MailOptions mailOptions = MailOptions(
+        body: finalText,
+        subject: subject,
+        recipients: [companyEmail],
+        isHTML: false);
 
     await FlutterMailer.send(mailOptions);
 
@@ -67,26 +74,78 @@ class _FeedBackScreen extends State<FeedBackScreen> {
 
     //android -> isAppInstalled will return false in IOS
     const GMAIL_SCHEMA = 'com.google.android.gm';
-    final bool gmailinstalled = await FlutterMailer.isAppInstalled(GMAIL_SCHEMA);
+    final bool gmailinstalled =
+        await FlutterMailer.isAppInstalled(GMAIL_SCHEMA);
     print("gmail: $gmailinstalled");
     if (gmailinstalled) {
       final MailerResponse response = await FlutterMailer.send(mailOptions);
       if (response == MailerResponse.android) {
-        //TODO: succsses snack bar
+        SnackBar snackBar = SnackBar(
+          duration: Duration(seconds: 2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          behavior: SnackBarBehavior.floating,
+          content:
+              const Text('Mail sent successfully', textAlign: TextAlign.center),
+          width: MediaQuery.of(context).size.width * 0.5,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        Navigator.of(context).pushReplacementNamed(TabsScreen.routeName);
+      }
+    } else {
+      //IOS
+      final bool canSend = await FlutterMailer.canSendMail();
+      print("can send: $canSend");
+      if (!canSend && Platform.isIOS) {
+        final url = "mailto:$companyEmail?subject=$subject&body=$finalText";
+        if (await canLaunch(url)) {
+          await launch(url);
+          SnackBar snackBar = SnackBar(
+            duration: Duration(seconds: 2),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            behavior: SnackBarBehavior.floating,
+            content: const Text('Mail sent successfully',
+                textAlign: TextAlign.center),
+            width: MediaQuery.of(context).size.width * 0.5,
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          Navigator.of(context).pushReplacementNamed(TabsScreen.routeName);
+        } else {
+          throw 'Could not launch $url';
+        }
       }
     }
+  }
 
-    //IOS
-    final bool canSend = await FlutterMailer.canSendMail();
-    print("can send: $canSend");
-    if (!canSend && Platform.isIOS) {
-      final url = "mailto:$companyEmail?subject=$subject&body=$finalText";
-      if (await canLaunch(url)) {
-        await launch(url);
-        //TODO: succsses for snack bar
-      } else {
-        throw 'Could not launch $url';
-      }
+  void _exitWithoutSavingDialog() {
+    if (_formChanged) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('Are your sure?'),
+          content: Text("You are about to exit without saving your changes."),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(ctx).pop();
+                Navigator.of(ctx).pop();
+              },
+            ),
+          ],
+        ),
+      );
+    } else {
+      Navigator.of(context).pop();
     }
   }
 
@@ -94,67 +153,91 @@ class _FeedBackScreen extends State<FeedBackScreen> {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Builder(
-        builder: (context) => Scaffold(
-          appBar: AppBar(
-            title: Text('Yroz FeedBack'),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => _exitWithoutSavingDialog(),
           ),
-          body: Directionality(
-            textDirection: TextDirection.ltr,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text("Please rate the following:", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  SizedBox(
-                    height: deviceSize.height * 0.05,
-                  ),
-                  Row(
-                    children: [
-                      Text("The app is user friendly"),
-                      _ratingBar(0),
-                    ],
-                  ),
-                  SizedBox(
-                    height: deviceSize.height * 0.05,
-                  ),
-                  Row(
-                    children: [
-                      Text("The app did not crash"),
-                      _ratingBar(1),
-                    ],
-                  ),
-                  SizedBox(
-                    height: deviceSize.height * 0.05,
-                  ),
-                  Row(
-                    children: [
-                      Text("The app responds fast"),
-                      _ratingBar(2),
-                    ],
-                  ),
-                  SizedBox(
-                    height: deviceSize.height * 0.05,
-                  ),
-                  Row(
-                    children: [
-                      Text("The app's design"),
-                      _ratingBar(3),
-                    ],
-                  ),
-                  SizedBox(
-                    height: deviceSize.height * 0.05,
-                  ),
-                  Container(
-                    width: deviceSize.width * 0.95,
-                    child: TextField(
+          toolbarHeight: deviceSize.height * 0.1,
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Rate Us',
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: SizedBox(
+              height: deviceSize.height * 0.85,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    left: deviceSize.width * 0.05,
+                    right: deviceSize.width * 0.05),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                        "Your feedback is important for us, please rate the following:",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              bottom: deviceSize.height * 0.015),
+                          child: Text(questions[0], textAlign: TextAlign.left),
+                        ),
+                        Center(child: _ratingBar(0)),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              bottom: deviceSize.height * 0.015),
+                          child: Text(questions[1], textAlign: TextAlign.left),
+                        ),
+                        Center(child: _ratingBar(1)),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              bottom: deviceSize.height * 0.015),
+                          child: Text(questions[2], textAlign: TextAlign.left),
+                        ),
+                        Center(child: _ratingBar(2)),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              bottom: deviceSize.height * 0.015),
+                          child: Text(questions[3], textAlign: TextAlign.left),
+                        ),
+                        Center(child: _ratingBar(3)),
+                      ],
+                    ),
+                    TextField(
                       controller: _textEditingController,
                       keyboardType: TextInputType.multiline,
-                      minLines: 1,
-                      maxLines: 10,
+                      maxLines: 3,
                       decoration: InputDecoration(
                         hintText: 'Please tell us more',
                         hintStyle: TextStyle(color: Colors.grey),
@@ -163,35 +246,44 @@ class _FeedBackScreen extends State<FeedBackScreen> {
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: deviceSize.height * 0.05,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        primary: Theme.of(context).primaryColor,
                       ),
-                      primary: Theme.of(context).primaryColor,
-                    ),
-                    child: Container(
-                      margin: EdgeInsets.all(deviceSize.width * 0.025),
-                      child: const Text(
-                        'Submit',
-                        style: TextStyle(
-                          color: Colors.white,
+                      child: Container(
+                        margin: EdgeInsets.all(deviceSize.width * 0.025),
+                        child: const Text(
+                          'Submit',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
+                      onPressed: () async {
+                        if (_ratings.isEmpty &&
+                            _textEditingController.text.isEmpty) {
+                          SnackBar snackBar = SnackBar(
+                            duration: Duration(seconds: 2),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            behavior: SnackBarBehavior.floating,
+                            content: const Text(
+                                'Please fill at least one field',
+                                textAlign: TextAlign.center),
+                            width: MediaQuery.of(context).size.width * 0.5,
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          return;
+                        }
+                        await sendEmail();
+                      },
                     ),
-                    onPressed: () async {
-                      print("submit");
-                      print(_ratings);
-                      print(_textEditingController.text);
-                      await sendEmail();
-                      Navigator.of(context).pushReplacementNamed(TabsScreen.routeName);
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
@@ -240,6 +332,7 @@ class _FeedBackScreen extends State<FeedBackScreen> {
         setState(() {
           _ratings.removeWhere((element) => element.item1 == questionIndex);
           _ratings.add(Tuple2(questionIndex, rating.toInt()));
+          _formChanged = true;
         });
       },
       updateOnDrag: true,
