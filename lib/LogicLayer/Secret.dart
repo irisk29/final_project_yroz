b7e64d93aa33559d1980f2f90a5790bf7964434f
Iris Kronfeld <iris.kronfeld@gmail.com>
2022-04-25 16:42:18 +0300

    use different url for store and product pictures

--- lib/LogicLayer/Secret.dart
+++ lib/LogicLayer/Secret.dart
@@ -3,10 +3,15 @@ class Secret {
   final String IV;
   final String API_KEY;
   final String COMPANY_EMAIL;
+  final String S3_URL;
 
-  Secret({this.KEY = "", this.IV = "", this.API_KEY = "", this.COMPANY_EMAIL = ""});
+  Secret({this.KEY = "", this.IV = "", this.API_KEY = "", this.COMPANY_EMAIL = "", this.S3_URL = ""});
   factory Secret.fromJson(Map<String, dynamic> jsonMap) {
     return new Secret(
-        KEY: jsonMap["KEY"], IV: jsonMap["IV"], API_KEY: jsonMap["API_KEY"], COMPANY_EMAIL: jsonMap["COMPANY_EMAIL"]);
+        KEY: jsonMap["KEY"],
+        IV: jsonMap["IV"],
+        API_KEY: jsonMap["API_KEY"],
+        COMPANY_EMAIL: jsonMap["COMPANY_EMAIL"],
+        S3_URL: jsonMap["S3_URL"]);
   }
 }
