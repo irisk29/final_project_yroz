29d39890c692e16a67be14a9b32f4aac93d95f08
sagivma <sagivm01@gmail.com>
2022-04-26 21:24:18 +0300

    start add route to manage

--- lib/widgets/default_store_item.dart
+++ lib/widgets/default_store_item.dart
@@ -1,25 +1,65 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:final_project_yroz/DTOs/OnlineStoreDTO.dart';
 import 'package:final_project_yroz/DTOs/StoreDTO.dart';
+import 'package:final_project_yroz/LogicLayer/User.dart';
 import 'package:final_project_yroz/screens/online_store_screen.dart';
 import 'package:final_project_yroz/screens/physical_store_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/manage_online_store_screen.dart';
+import '../screens/manage_physical_store_screen.dart';
 
 class StoreItem extends StatelessWidget {
   final StoreDTO store;
+  VoidCallback callback;
 
-  StoreItem(this.store);
+  StoreItem(this.store, this.callback);
+
+  void showManageMyStoreDialog(BuildContext ctx) {
+    SnackBar snackBar = SnackBar(
+      duration: Duration(seconds: 5),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      backgroundColor: Theme.of(ctx).primaryColor,
+      behavior: SnackBarBehavior.floating,
+      content: const Text(
+          'You chose your store, you can see how other users see it using "Store Preview" option',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black87)),
+      width: MediaQuery.of(ctx).size.width * 0.75,
+    );
+    ScaffoldMessenger.of(ctx).showSnackBar(snackBar);
+  }
 
   void selectStore(BuildContext ctx) {
-    this.store is OnlineStoreDTO
-        ? Navigator.of(ctx).pushNamed(
-            OnlineStoreScreen.routeName,
-            arguments: {'store': store},
-          )
-        : Navigator.of(ctx).pushNamed(
-            PhysicalStoreScreen.routeName,
-            arguments: {'store': store},
-          );
+    final user = Provider.of<User>(ctx);
+
+    if (user.storeOwnerState != null) {
+      user.storeOwnerState!.hasPhysicalStore(store.id)
+          ? Navigator.of(ctx)
+              .pushNamed(ManagePhysicalStoreScreen.routeName)
+              .then((value) {
+              if (value != null) callback();
+            })
+          : Navigator.of(ctx)
+              .pushNamed(ManageOnlineStoreScreen.routeName)
+              .then((value) {
+              if (value != null) callback();
+            });
+      showManageMyStoreDialog(ctx);
+    } else {
+      this.store is OnlineStoreDTO
+          ? Navigator.of(ctx).pushNamed(
+              OnlineStoreScreen.routeName,
+              arguments: {'store': store},
+            )
+          : Navigator.of(ctx).pushNamed(
+              PhysicalStoreScreen.routeName,
+              arguments: {'store': store},
+            );
+    }
   }
 
   @override
