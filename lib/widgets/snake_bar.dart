c7d65977e27e1d6964ba083a958f7c29e8f7ab3d
Iris Kronfeld <iris.kronfeld@gmail.com>
2022-05-17 22:26:36 +0300

    remove redundant file

--- lib/widgets/snake_bar.dart
+++ lib/widgets/snake_bar.dart
@@ -1,17 +1,7 @@
-import 'package:final_project_yroz/LogicLayer/ConnectivityProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-SnackBar showSnackBar(BuildContext context, String content) {
-  final isOnline = Provider.of<ConnectivityProvider>(context).isOnline;
-
-  String msg = content;
-
-  if (!isOnline) {
-    msg +=
-        " BUT be aware you have no internet connection, your acction will be updated to other users once you restore your internet.";
-  }
-
+void showSnackBar(BuildContext context, String content) {
   SnackBar snackBar = SnackBar(
     duration: Duration(seconds: 3),
     shape: RoundedRectangleBorder(
@@ -19,11 +9,9 @@ SnackBar showSnackBar(BuildContext context, String content) {
     ),
     backgroundColor: Theme.of(context).primaryColor,
     behavior: SnackBarBehavior.floating,
-    content: Text(msg, textAlign: TextAlign.center, style: TextStyle(color: Colors.black87)),
+    content: Text(content, textAlign: TextAlign.center, style: TextStyle(color: Colors.black87)),
     width: MediaQuery.of(context).size.width * 0.75,
   );
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
   Navigator.of(context).pop();
-
-  return snackBar;
 }
